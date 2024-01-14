c7b94efb6b7cf25d4e07bbddb96b5e536f8ab4ed
Chinmay Walinjkar <100498663+chinmay4613@users.noreply.github.com>
2024-01-14 18:33:40 +0530

    Update sym.py

--- w2/sym.py
+++ w2/sym.py
@@ -1,1 +1,30 @@
+class SYM:
+    def __init__(self, s=None, n=None):
+        self.txt = s or " "
+        self.at = n or 0
+        self.n = 0
+        self.has = {}
+        self.mode = None
+        self.most = 0
 
+    def add(self, x):
+        if x != "?":
+            self.n += 1
+            self.has[x] = 1 + (self.has.get(x) or 0)
+            if self.has[x] > self.most:
+                self.most, self.mode = self.has[x], x
+
+    def mid(self):
+        return self.mode
+
+    def div(self):
+        e = 0
+        for v in self.has.values():
+            e = e - v / self.n * math.log(v / self.n, 2)
+        return e
+
+    def small(self):
+        return 0
+
+    def like(self, x, prior):
+        return ((self.has.get(x) or 0) + the["m"] * prior) / (self.n + the["m"])
